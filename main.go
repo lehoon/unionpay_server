4d3dc3bd8d3e951e5f5c9159ca6a78ee7caad576
lehoon <lehoons@gmail.com>
2023-02-20 17:51:59 +0800

    add last modify code

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/lehoon/unionpay_server/library/config"
@@ -14,6 +15,9 @@ func main() {
 	fmt.Printf("发送测试数据:%s\n", config.GetServerContent())
 	fmt.Printf("目标地址:%s\n", config.GetRestUrl())
 
+	var rsp string
+	var err error
+
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 	for {
@@ -21,7 +25,21 @@ func main() {
 		case <-ticker.C:
 			{
 				bNeedNewLine := false
-				rsp, err := http.PostStringContent(config.GetRestUrl(), config.GetServerContent(), "text/html; charset=utf-8")
+				r := rand.New(rand.NewSource(time.Now().UnixNano()))
+				fmt.Printf("%d\r\n", r.Uint32())
+				if r.Uint32()%4 == 0 {
+					fmt.Println("发送post请求")
+					rsp, err = http.PostStringContent(config.GetRestUrl(), config.GetServerContent(), "text/html; charset=utf-8")
+				} else if r.Uint32()%4 == 1 {
+					fmt.Println("发送get请求" + config.GetRestUrl())
+					rsp, err = http.Get(config.GetRestUrl())
+				} else if r.Uint32()%4 == 2 {
+					fmt.Println("发送post请求http://localhost:9528/pay/gateway")
+					rsp, err = http.PostStringContent("http://localhost:9528/pay/gateway", config.GetServerContent(), "text/html; charset=utf-8")
+				} else if r.Uint32()%4 == 3 {
+					fmt.Println("发送get请求http://localhost:9528/pay/gateway")
+					rsp, err = http.Get("http://localhost:9528/pay/gateway")
+				}
 
 				if err != nil {
 					bNeedNewLine = true
